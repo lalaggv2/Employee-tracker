1a0a526a62c0dc5e9ec797d063103a8213a6eb13
lalaggv2 <luangu2020@gmail.com>
2020-06-15 23:53:20 -0400

    starter code

--- squema.sql
+++ squema.sql
@@ -3,19 +3,18 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE employee
+CREATE TABLE id
 (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+ id INT PRIMARY KEY,
+dpt_name VARCHAR(30) to hold department name
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE role
+(
+  id - INT PRIMARY KEY,
+  title 
+)
 
 SELECT *
 FROM top5000;
