6568e760a557939572480dce407c52596ebb548f
Lala <lalagg@hotmail.com>
2020-06-21 18:30:54 -0400

    add employee, view employee and add role working

--- db/Database.js
+++ db/Database.js
@@ -11,60 +11,40 @@ class Database {
       // SELECT ...LO QUE SEA LA QUERY
       `INSERT INTO departments (dpt_name) VALUES ("${departmentName}")`
     );
-  }
+  };
+};
 
+//   createRole() {
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
 
-  createEmployee(newEmployee) {
-    return this.connection.query(
-      `INSERT INTO employee (first_name, last_name, role_id, manager_id)) VALUES ("${newEmployee}")`
-      // first_name: answer.employee_first, 
-      // last_name: answer.employee_last,
-      // role_id: answer.role,
-      // manager_id: answer.manager,
-
-    );
-  }
-
-
-  createRole() {
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-
-  findDepartments() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-  findEmployees() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-  findRoles() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-  // y otro para update
-  updateDepartments() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-  updateEmployees() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-  updateRoles() {//y los mismo para los demas
-    return this.connection.query(
-      // SELECT ...LO QUE SEA LA QUERY
-    );
-  }
-}
-
-
-
+//   findEmployees() {//y los mismo para los demas
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
+//   findRoles() {//y los mismo para los demas
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
+//   // y otro para update
+//   updateDepartments() {//y los mismo para los demas
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
+//   updateEmployees() {//y los mismo para los demas
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
+//   updateRoles() {//y los mismo para los demas
+//     return this.connection.query(
+//       // SELECT ...LO QUE SEA LA QUERY
+//     );
+//   }
+// }
 module.exports = new Database(connection);
--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 require('dotenv').config();
-//require inquire and console.table
-//require the database class
-//const express = require("express");
+
 const inquirer = require("inquirer");
-const console = require("console.table");
+const cTable = require("console.table");
 const mysql = require("mysql");
 const db = require("./db/Database");
 const { updateDepartments, updateRoles } = require('./db/Database');
+const connection = require('./db/connection');
 // const { type } = require("os");
 
 
 //create a prommpt when the app is started to ask the user what he wants to do
 //choices add dept, add role, addemployee
 
-// connection.connect(function (err) {
-//   if (err) throw err;
-//   runSearch();
-// });
 //esta es la que hace las preguntas
 runSearch();
 
@@ -33,9 +28,9 @@ function runSearch() {
         'View Departments',
         'View roles',
         'View employees',
-        'Update Departments',
-        'Update roles',
-        'Update employee',
+        'Update employee roles',
+        'Remove employee',
+        'Remove department',
         'exit',
       ],
     })
@@ -49,32 +44,28 @@ function runSearch() {
           viewDepartments()
           break;
 
-        case 'Add Employee':
-          addEmployee()
+        case 'Add a new employee':
+          addNewEmployee()
           break;
 
-        case 'View Employee':
-          viewEmployee()
+        case 'Add role':
+          addNewRole()
           break;
 
-        case 'Add roles':
-          addRoles()
+        case 'View Employees':
+          seeEmployee()
+          break;
+
+        case 'Remove Employee':
+          deleteEmployee()
+          break;
+
+        case 'Update Employee':
+          updateEmployee()
           break;
 
         case 'View roles':
           viewRoles()
-          break;
-
-        case 'Update Departments':
-          updateDepartments()
-          break;
-
-        case 'Update Employee':
-          updateEmployees()
-          break;
-
-        case 'Update Roles':
-          updateRoles()
           break;
 
         case 'exit':
@@ -97,65 +88,235 @@ function addDepartment() {
 };
 
 function viewDepartments() {
-  inquirer.prompt({
-    name: 'name',
-    type: 'text',
-    message: 'Which department do you want to see?'
-  }).then(answer => {
-    db.viewDepartment(answer.name)
-    runSearch()
+  connection.query("SELECT * FROM departments", (err, result) => {
+    if (err) throw err;
+    console.table(result);
+    runSearch();
   })
-};
+}
 
-function addEmployee() {
-  inquirer
-    .prompt({
-      name: 'employee_first',
-      type: 'input',
-      message: "What's the first name of the employee you would like to add?"
-    }, {
-      name: "employee last",
-      type: 'input',
-      message: 'What is the last name of the employee?'
+// function updateDpts() {
+//   inquirer.prompt({
+//     name: 'name',
+//     type: 'text',
+//     message: 'Which department do you want to update?'
+//   },
+//     {
+//       name: "new department",
+//       type: 'text',
+//       message: "What's the new name of the department?",
+//     }).then(answer => {
+//       db.updateDepartment(answer)
+//       runSearch;
+//     });
+// };
+
+
+function addNewEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's first name?",
+      name: "first_name"
     },
-      {
-        name: 'role',
-        type: 'list',
-        message: "What's the employee role?",
-        choices: [Finance, IT, Marketing, HR, Sales, Manufacturing, Engineering]
-      },
-      {
-        name: 'manager',
-        type: 'list',
-        message: "Who's the employee manager?",
-      })
-    .then(answer => {
-      db.createEmployee(answer.employee_first)
-      runSearch()
-    })
-};
-
-function updateEmployeeRole() {
-  inquirer
-    .prompt({
-      name: "employee name",
-      type: "list",
-      message: "which employee would you like to update?"
-      choices: ["employee.id"]
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "last_name"
     },
-      {
-        name: "update role",
-        type: "list",
-        message: "What's the new role of the employee you would like to update?",
-        choices: ['roles.title']
-      })
-    .then((answer) => {
-      updateRoles()
-    })
-};
-
-function addRole() {
-  inquirer.prompt({
-    name: 
+    {
+      type: "input",
+      message: "What is the employee's role id number?",
+      name: "role_id"
+    },
+    {
+      type: "input",
+      message: "What is the employee's manager ID number?",
+      name: "manager_id"
+    }
+  ]).then(answer => {
+    connection.query("INSERT INTO employee_trackerdb.employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [
+      answer.first_name,
+      answer.last_name,
+      answer.role_id,
+      answer.manager_id,
+    ], (err, res) => {
+      if (err) throw err;
+      // console.log("Employee has been added:)");
+      runSearch();
+    });
   });
 };
+
+function addNewRole() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "What is the salary?",
+      name: "salary"
+    },
+    {
+      type: "input",
+      message: "What is the department ID this role belongs to?",
+      name: "department_id",
+      //choices: ["IT", 'Finance', "Marketing", "Operations", "Janitorial", "Payroll", "Drafting"]
+    }
+  ]).then(answer => {
+    connection.query("INSERT INTO employee_trackerdb.roles(title, salary, department_id) VALUES (?, ?, ?)", [
+      answer.title,
+      answer.salary,
+      answer.department_id,
+    ], (err, res) => {
+      if (err) throw err;
+      runSearch();
+    });
+  });
+};
+
+
+function seeEmployee() {
+  connection.query("SELECT * FROM employee", (err, result) => {
+    if (err) throw err;
+    console.table(result);
+    runSearch();
+  });
+};
+
+function viewRoles() {
+  connection.query("SELECT * FROM roles", (err, result) => {
+    if (err) throw err;
+    console.table(result);
+    runSearch();
+  })
+}
+
+function updateEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Which employee ID would you like to update?",
+      name: "employeeid"
+    },
+    {
+      type: "input",
+      message: "What is the employee first name?",
+      name: "employeefirstname"
+    },
+    {
+      type: "input",
+      message: "What is the employee last name?",
+      name: "employeelastname"
+    },
+    {
+      type: "input",
+      message: "What is the employee new role id?",
+      name: "role_id"
+    },
+    {
+      type: "input",
+      message: "What is the employee new manager id?",
+      name: "manager_id"
+    }
+  ]).then(answer => {
+    connection.query("UPDATE employee_trackerdb.employee SET first_name =?, last_name =?, role_id=?, manager_id=? WHERE id=?",
+      [
+        answer.employeefirstname,
+        answer.employeelastname,
+        answer.role_id,
+        answer.manager_id,
+        answer.employeeid
+      ], (err, res) => {
+        if (err) throw err;
+        //console.log("Employee has been updated:)");
+        runSearch();
+      });
+  })
+}
+
+function deleteEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What employee would you like to delete?",
+      name: "empdelete"
+    }
+  ]).then(res => {
+    connection.query("DELETE from employee_tracker.employee WHERE id=?",
+      [
+        res.empdelete
+      ], (err, res) => {
+        if (err) throw err;
+        console.log("Employee has been deleted:)");
+        runSearch();
+      })
+  })
+}
+
+// function viewAllEmployees() {
+//   connection.query(
+//     "SELECT employee.id, employee.first_name, employee.last_name, employee_role.title, department.name AS dept FROM employee JOIN employee_role ON employee_role.id = employee.role_id JOIN department on employee_role.department_id = department.id;",
+//     (err, res) => {
+//       if (err) throw err;
+//       console.table(res);
+//       runSearch();
+//     }
+//   );
+// }
+
+// function addEmployee() {
+//   inquirer
+//     .prompt({
+//       name: 'employee_first',
+//       type: 'input',
+//       message: "What's the first name of the employee you would like to add?"
+//     }, {
+//       name: "employee last",
+//       type: 'input',
+//       message: 'What is the last name of the employee?'
+//     },
+//       {
+//         name: 'role',
+//         type: 'list',
+//         message: "What's the employee role?",
+//         choices: [Finance, IT, Marketing, HR, Sales, Manufacturing, Engineering]
+//       },
+//       {
+//         name: 'manager',
+//         type: 'list',
+//         message: "Who's the employee manager?",
+//       })
+//     .then(answer => {
+//       db.createEmployee(answer.employee_first)
+//       runSearch()
+//     })
+// };
+
+// function updateEmployeeRole() {
+//   inquirer
+//     .prompt({
+//       name: "employee name",
+//       type: "list",
+//       message: "which employee would you like to update?"
+//       //  choices: ["employee.id"]
+//     },
+//       {
+//         name: "update role",
+//         type: "list",
+//         message: "What's the new role of the employee you would like to update?",
+//         //    choices: ['roles.title']
+//       })
+//     .then((answer) => {
+//       updateRoles()
+//     })
+// };
+
+// function addRole() {
+//   inquirer.prompt({
+//     name: 
+//   });
+// };
